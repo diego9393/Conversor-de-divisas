bcc0e0a76b4f0aec94c1ab732c9bae8a8809d1bf
diego <diegomadrid100@outlook.com>
2018-10-19 09:24:40 +0200

    creacion de calses necesarias

--- Modelo/FactorConversion.cs
+++ Modelo/FactorConversion.cs
@@ -3,15 +3,18 @@
     public class FactorConversion
     {
         public int Id { get; set; }
-        public Moneda MonedaOrigen { get; set; }
-        public Moneda MonedaDestino { get; set; }
+        public Moneda IdMonedaOrigen { get; set; }
+        public Moneda IdMonedaDestino { get; set; }
         public decimal Factor { get; set; }
+
+        public Moneda MonedaOrigen { get; set; }
+        public Moneda monedaDestino { get; set; }
 
         public FactorConversion(Moneda monedaOrigen,
             Moneda monedaDestino, decimal factor)
         {
-            MonedaOrigen = monedaOrigen;
-            MonedaDestino = monedaDestino;
+            IdMonedaOrigen = monedaOrigen;
+            IdMonedaDestino = monedaDestino;
             Factor = factor;
         }
     }
