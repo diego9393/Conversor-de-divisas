3d66e57b1976c0221d175f853526ba042144ad6e
diego <diegomadrid100@outlook.com>
2018-10-19 08:21:11 +0200

    primer commit
    funcuona la api

---
+++ proyecto final/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proyecto_final.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var valor = new jsonParser();
+            valor.from = "USD";
+            valor.to = "EUR";
+            valor.cantidad = "100";
+            var json = valor.getValor();
+            ViewBag.Message = json;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
